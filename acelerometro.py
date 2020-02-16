794a32c1443b5fd2b45e642970857b9177ead149
juancalf <60267220+juancalf@users.noreply.github.com>
2020-02-16 18:19:52 +0100

    pitch roll cambio de orden

--- acelerometro.py
+++ acelerometro.py
@@ -28,8 +28,8 @@ class MPU:
 		acc_x = self.mpu.accelerometer_data[0]
 		acc_y = self.mpu.accelerometer_data[1]
 		acc_z = self.mpu.accelerometer_data[2]
-		roll = math.degrees(math.atan(acc_y/acc_z))
-		pitch = math.degrees(math.atan((-acc_x)/math.sqrt(acc_y**2 + acc_z**2)))
+		pitch = math.degrees(math.atan(acc_y/acc_z))
+		roll = math.degrees(math.atan((-acc_x)/math.sqrt(acc_y**2 + acc_z**2)))
 		return pitch, roll
 
 	def getPitchRollRad(self):
@@ -37,6 +37,6 @@ class MPU:
 		acc_x = self.mpu.accelerometer_data[0]
 		acc_y = self.mpu.accelerometer_data[1]
 		acc_z = self.mpu.accelerometer_data[2]
-		roll = math.atan(acc_y/acc_z)
-		pitch = math.atan((-acc_x)/math.sqrt(acc_y**2 + acc_z**2))
+		pith = math.atan(acc_y/acc_z)
+		roll = math.atan((-acc_x)/math.sqrt(acc_y**2 + acc_z**2))
 		return pitch, roll
