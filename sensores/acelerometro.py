7ac5ff037fd3a725f58eae21296536ae8751b663
juancalf <60267220+juancalf@users.noreply.github.com>
2020-03-02 17:37:03 +0100

    final
    si va mal realizar una calibración

--- sensores/acelerometro.py
+++ sensores/acelerometro.py
@@ -1,38 +1,54 @@
+## Usamos el sensor LSM9DS1 en lugar del MPU9250
+
 import spidev
 import time
 import argparse
 import sys
 import navio.util
+import datetime
 from Complementary_Filter import comp_filt
 
-navio.util.check_apm()
+class Acelerometro:
 
-posicion = comp_filt()
-posicion.init()
+    #imu = navio.mpu9250.MPU9250()
+    imu = navio.lsm9ds1.LSM9DS1()
+    posicion = comp_filt()
 
-#De los dos sensores usamos el lsm que es mas preciso
-#imu = navio.mpu9250.MPU9250()
-imu = navio.lsm9ds1.LSM9DS1()
+    def __init__(self):
+        navio.util.check_apm()
 
-if imu.testConnection():
-    print "Connection established: True"
-else:
-    sys.exit("Connection established: False")
+        if self.imu.testConnection():
+            print "conexion establecida con el imu"
+        else:
+            sys.exit("error, conexion no establecida con el imu")
 
-imu.initialize()
-time.sleep(1)
+        self.imu.initialize()
+        time.sleep(1)
 
-while 1:
-	m9a, m9g, m9m = imu.getMotion9()
+        self.posicion.init()
 
-	posicion.attitude3(float(m9a[0]),float(m9a[1]),float(m9a[2]),float(m9g[0]),float(m9g[1]),
-		float(m9g[2]),float(m9m[0]),float(m9m[1]),float(m9m[2]))
+    def getPitchRollYaw(self):
+        m9a, m9g, m9m = self.imu.getMotion9()
 
-	roll = -posicion.pitch_d
-	pitch = -posicion.roll_d
-	yaw = posicion.yaw_d
-	print "pitch", pitch
-	print "roll", roll
-	print "yaw" , yaw
-	posicion.reset()
-	time.sleep(0.5)
+        self.posicion.attitude3(float(m9a[0]),float(m9a[1]),float(m9a[2]),float(m9g[0]),float(m9g[1]),
+        	float(m9g[2]),float(m9m[0]),float(m9m[1]),float(m9m[2]))
+
+        #por la posicion del sensor hacemos los siguientes cambios
+        roll = -self.posicion.pitch_d
+        pitch = -self.posicion.roll_d
+        yaw = self.posicion.yaw_d
+
+        self.posicion.reset()
+        return pitch, roll, yaw;
+
+    """funcion de depuracion, muestra pitch, roll y yaw por pantalla hasta
+    interrumpir por teclado"""
+    def bucleTest(self):
+        while True:
+            pitch, roll, yaw = self.getPitchRollYaw()
+
+            print "pitch", pitch
+            print "roll", roll
+            print "yaw" , yaw
+
+            time.sleep(0.5)
