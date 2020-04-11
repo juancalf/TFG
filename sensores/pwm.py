3f93a44f025d5a5f61502cc74275a109823739b5
juancalf <60267220+juancalf@users.noreply.github.com>
2020-04-11 22:02:26 +0200

    final

--- sensores/pwm.py
+++ sensores/pwm.py
@@ -1,4 +1,3 @@
-# primera version inacabada
 import sys
 import time
 import threading
@@ -14,20 +13,20 @@ class Pwm:
     ESC_INPUT_4 = 10
 
     # controladores pwm
-    pwm_1 =  navio.pwm.PWM(ESC_INPUT_1)
-    pwm_2 =  navio.pwm.PWM(ESC_INPUT_2)
-    pwm_3 =  navio.pwm.PWM(ESC_INPUT_3)
-    pwm_4 =  navio.pwm.PWM(ESC_INPUT_4)
+    pwm_1 =  navio.pwm.PWM(self.ESC_INPUT_1)
+    pwm_2 =  navio.pwm.PWM(self.ESC_INPUT_2)
+    pwm_3 =  navio.pwm.PWM(self.ESC_INPUT_3)
+    pwm_4 =  navio.pwm.PWM(self.ESC_INPUT_4)
 
     # valores de throttle
     SERVO_MIN = 1.000 #ms -> 0%
     SERVO_MAX = 2.000 #ms -> 100%
 
     # valores de throttle actuales (por defecto empiezan apagados)
-    motor1 = SERVO_MIN
-    motor2 = SERVO_MIN
-    motor3 = SERVO_MIN
-    motor4 = SERVO_MIN
+    motor1 = self.SERVO_MIN
+    motor2 = self.SERVO_MIN
+    motor3 = self.SERVO_MIN
+    motor4 = self.SERVO_MIN
 
     # otras variables
     calibrado = False 
@@ -54,38 +53,42 @@ class Pwm:
         self.pwm_3.enable()
         self.pwm_4.enable()
 
-    """funcion de calibracion, se debe llamar antes de usar los motores"""   
-    def calibracion(self):
-        self.aplicarThrottle(2.000,2.000,2.000,2.000) # 100%
-        print ("ESCs nivel maximo")
-        print("conectar bateria")
-        time.sleep(10) ## esperamos 10s a que los variadores confirmen
-        self.aplicarThrottle(1.000,1.000,1.000,1.000) # 0% 
-        print ("ESCs nivel minimo")
-        time.sleep(10)
-        self.aplicarThrottle(1.100,1.100,1.100,1.100) # 10%
-        print ("calibracion completada")
-        time.sleep(5)
-        self.aplicarThrottle(1.000,1.000,1.000,1.000)
-
-
+    """fucion paralela para el movimiento de los motores"""
     def _loop_(self):
-        while funcionando:
+        while self.funcionando:
             self.pwm_1.set_duty_cycle(self.motor1)
             self.pwm_2.set_duty_cycle(self.motor2)
             self.pwm_3.set_duty_cycle(self.motor3)
             self.pwm_4.set_duty_cycle(self.motor4)
 
+    """funcion usada para variar la velocidad de los motores"""
     def aplicarThrottle(self,m1,m2,m3,m4):
         self.motor1 = self.m1
         self.motor2 = self.m2
         self.motor3 = self.m3
         self.motor4 = self.m4
 
+    """funcion para arrancar los motores, llama a _loop_ en otro hilo"""
     def conectarMotores(self):
         self.funcionando = True
         self.thread.start()
 
+    """funcion de calibracion, se debe llamar antes de usar los motores"""   
+    def calibracion(self):
+        self.aplicarThrottle(self.SERVO_MAX,self.SERVO_MAX,self.SERVO_MAX,self.SERVO_MAX) # 100%
+        print ("ESCs nivel maximo")
+        print("conectar bateria")
+        time.sleep(10) ## esperamos 10s a que los variadores confirmen
+        self.aplicarThrottle(self.SERVO_MIN,self.SERVO_MIN,self.SERVO_MIN,self.SERVO_MIN) # 0% 
+        print ("ESCs nivel minimo")
+        time.sleep(10)
+        self.aplicarThrottle(1.100,1.100,1.100,1.100) # 10%
+        print ("calibracion completada")
+        time.sleep(5)
+        #apagamos motores
+        self.aplicarThrottle(self.SERVO_MIN,self.SERVO_MIN,self.SERVO_MIN,self.SERVO_MIN)
+
+    """usada al finalizar el recorrido para desconectar el controlador pwm"""
     def desconectarMotores(self):
         self.funcionando = False
         self.thread.join()
